a1d8209bb2b3e6412c1e4c1c9375083581ab830d
samamoto <31977136+samamoto@users.noreply.github.com>
2017-09-15 18:25:23 +0900

    プロトタイプ

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    //テスト用
+    public void _DebugInput()
+    {
+        if (Input.GetKeyUp("1"))
+        {
+            _SceneLoadTitle();
+        }
+
+        if (Input.GetKeyUp("2"))
+        {
+            _SceneLoadGame();
+        }
+
+        if (Input.GetKeyUp("3"))
+        {
+            _SceneLoadResult();
+        }
+    }
+
+    //Scene変更
+    public void _SceneLoadTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+    public void _SceneLoadGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void _SceneLoadResult()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+}
