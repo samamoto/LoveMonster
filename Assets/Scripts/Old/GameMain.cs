fefa8a30a49e64900365d8aa723a9711e236cf99
samamoto <31977136+samamoto@users.noreply.github.com>
2017-10-08 09:58:33 +0900

    スクリプトの移動
    使用しないスクリプトを移動
    Oldに入れていく

---
+++ Assets/Scripts/Old/GameMain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    //スクリプト群
+    private SceneChange m_ScreenChange;
+
+    private InputManagerGenerator inputmanage;
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_ScreenChange = GameObject.Find("SceneChange").GetComponent<SceneChange>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_ScreenChange._DebugInput();
+    }
+}
---
+++ Assets/Scripts/Old/TitleMain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMain : MonoBehaviour {
+
+    //スクリプト群
+    SceneChange m_ScreenChange;
+
+    // Use this for initialization
+    void Start () {
+        m_ScreenChange = GameObject.Find("SceneChange").GetComponent<SceneChange>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_ScreenChange._DebugInput();
+    }
+}
