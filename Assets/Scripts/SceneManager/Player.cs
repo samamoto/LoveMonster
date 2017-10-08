ffe6cea50b9395f7b45f1ee318edc4c5fdb7b9e5
samamoto <31977136+samamoto@users.noreply.github.com>
2017-10-08 10:04:47 +0900

    スクリプト追加

---
+++ Assets/Scripts/SceneManager/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Rigidbody RB;
+    Vector3 velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity.z = Input.GetAxis("Vertical");
+
+        RB.AddForce(velocity * 20 * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "LargeSquare":
+                Debug.Log("LargeSquare");
+                break;
+            case "SmallSquare":
+                Debug.Log("SmallSquare");
+                break;
+            case "LargeRectAngle":
+                Debug.Log("LargeRectAngle");
+                break;
+            case "SmallRectAngle":
+                Debug.Log("SmallRectAngle");
+                break;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        switch(other.tag)
+        {
+            case "LargeSquare":
+
+                break;
+            case "SmallSquare":
+
+                break;
+            case "LargeRectAngle":
+
+                break;
+            case "SmallRectAngle":
+
+                break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "LargeSquare":
+
+                break;
+            case "SmallSquare":
+
+                break;
+            case "LargeRectAngle":
+
+                break;
+            case "SmallRectAngle":
+
+                break;
+        }
+    }
+}
