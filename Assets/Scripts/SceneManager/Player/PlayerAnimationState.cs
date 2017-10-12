24ec3ec9c664b0720bddacb99a21b538993d65e0
samamoto <31977136+samamoto@users.noreply.github.com>
2017-10-12 22:57:27 +0900

    アニメーションの状態遷移用スクリプト

---
+++ Assets/Scripts/SceneManager/Player/PlayerAnimationState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PLAYER_ANIMATION_STATE
+{
+    DEAD,
+    STAY,
+    DUSH,
+    JUMP,
+    VAULT,
+    CLIM,
+    SLIDE,
+    WALK_WALL,
+    FALL,//落下
+    LAND,//着地
+    BREAK_FALL,//受け身
+}
