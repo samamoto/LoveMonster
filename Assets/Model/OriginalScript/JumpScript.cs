7eb10576d42f051ef809bf36fddf244fbd60c83e
masuda <samamoto05@gmail.com>
2017-10-20 11:01:13 +0900

    仮のモデルを追加

---
+++ Assets/Model/OriginalScript/JumpScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            anim.SetBool("is_jumping", true);
+        }
+
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (state.IsName("Locomotion.Jump"))
+        {
+            anim.SetBool("is_jumping", false);
+        }
+    }
+}
