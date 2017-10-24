f3c96dab46f00b94a424194a5668e1a9d36d78c0
masuda <samamoto05@gmail.com>
2017-10-24 20:38:16 +0900

    マージミス？の修正

--- Assets/Scripts/SceneManager/Player/AllPlayerManager.cs
+++ Assets/Scripts/SceneManager/Player/AllPlayerManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 public class AllPlayerManager : MonoBehaviour {
 
     //走る速さ
@@ -14,19 +13,6 @@ public class AllPlayerManager : MonoBehaviour {
     //ジャンプ時間
     public float m_JumpTime = 2;
     // Use this for initialization
-=======
-public class AllPlayerManager : MonoBehaviour {
-
-    //走る速さ
-    public float m_RunSpeed = 0.2f;
-    public float m_SideRunSpeed = 0.05f;
-
-    //ジャンプ力
-    public float m_JumpPower = 1.0f;
-    //ジャンプ時間
-    public float m_JumpTime = 2;
-    // Use this for initialization
->>>>>>> 2c7d749b193a559bc6bd94a82a04f421c2dfabca
     void Start () {
 		
 	}
