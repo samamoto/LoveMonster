f3589ca91992cdacfa051d804bcbc38766a10ed6
OyamaAtsushi <blazwel@gmail.com>
2017-11-18 17:46:21 +0900

    大杉さんの更新分を導入

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+    private int minite;
+    private float second;
+    private int oldSecond;
+    private bool timerFlag= true;
+    private char textField;
+
+    float countTime = 0;
+
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        countTime += Time.deltaTime; //スタートしてからの秒数を格納
+        GetComponent<Text>().text = countTime.ToString("F2"); //小数2桁にして表示
+    }
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+/// <summary>
+/// それぞれのUIを管理するScript
+/// PlayerのUI用のScriptは表示のみ(中身の構成)を
+/// 行い、値の変動はこちらで管理する。
+/// </summary>
+
+
+public class UIManager : ScriptableObject
+{
+    [MenuItem("Tools/MyTool/Do It in C#")]
+    static void DoIt()
+    {
+        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+    }
+}
