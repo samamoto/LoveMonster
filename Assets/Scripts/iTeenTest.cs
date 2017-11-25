7fdeee0b70c2376966a3037d4d60a17ad07671ee
水薙優維 <thirddelta.yu_i501@me.com>
2017-11-25 15:23:13 +0900

    カメラをリプログラム

--- Assets/Scripts/iTeenTest.cs
+++ Assets/Scripts/iTeenTest.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class iTeenTest : MonoBehaviour {
+public class iTeenTest : MonoBehaviour
+{
+    public GameObject obj;
 
-	public GameObject obj;
+    // Use this for initialization
+    private void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//iTween.MoveFrom(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
-		//iTween.MoveTo(obj, iTween.Hash("x", obj.transform.position.x+1, "time", 3));
-		//iTween.RotateTo(obj, iTween.Hash("y", 90, "time", 3));
-		// ついてくる
-		iTween.MoveUpdate(gameObject, iTween.Hash("position", obj.transform.position, "time", 5));
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+        //iTween.MoveFrom(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+        //iTween.MoveTo(obj, iTween.Hash("x", obj.transform.position.x+1, "time", 3));
+        //iTween.RotateTo(obj, iTween.Hash("y", 90, "time", 3));
+        // ついてくる
+        //iTween.MoveUpdate(gameObject, iTween.Hash("position", obj.transform.position, "time", 5));
+    }
 }
