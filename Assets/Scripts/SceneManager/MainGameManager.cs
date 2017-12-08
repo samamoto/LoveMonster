acb3dc6e7bed8a65b4b7dc916981c4001773fc24
OyamaAtsushi <blazwel@gmail.com>
2017-12-08 14:33:45 +0900

    UIの配置調整

--- Assets/Scripts/SceneManager/MainGameManager.cs
+++ Assets/Scripts/SceneManager/MainGameManager.cs
@@ -91,7 +91,6 @@ public class MainGameManager : MonoBehaviour {
 				setPhaseState(PhaseLevel.Pause);
 			}
 			// Pauseメニュー表示 //
-
 			break;
 
 		//================================================================================
