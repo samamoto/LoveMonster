6a33859b4738820787992dedf37d264512501016
kaeruhito <31983465+kaeruhito@users.noreply.github.com>
2017-12-10 23:28:18 +0900

    se鳴らした

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -47,6 +47,9 @@ public class PlayerManager : MonoBehaviour, PlayerReciever
     private bool wallRunActivated = false; // set in Update() and used to activate wallRun in FixedUpdate()
     private bool wallRunTimeUp = false; // controlling flag for handling when wall run time times up
 
+    // 西田 SE用のDelay
+    private float m_seDelay = 0.0f;
+
     // Use this for initialization
     void Start() {
 		m_AllPlayerManager = GetComponentInParent<AllPlayerManager>();
@@ -126,15 +129,105 @@ public class PlayerManager : MonoBehaviour, PlayerReciever
 			// キーボードのほうは全員でジャンプする（キーボードはID管理してない）
 			if (m_Controller.GetButtonDown(Button.A) || Input.GetKeyDown(KeyCode.Space)) {
 				jump = true;
-			}
+
+                //地面にいる時だけ
+                if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                {
+                    m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionJump);
+                }        
+            }
 			m_animator.SetBool("is_Jump", jump);    // oyama add
 		}
+
+        //再生する関数
+        ActionSE();
     }
 
-	/// <summary>
-	/// ThirdPersonFixedUpdate
-	/// </summary>
-	void FixedUpdate() {
+    /// <summary>
+    /// アクションが再生される時にSEを鳴らす
+    /// </summary>
+    void ActionSE() {
+        //着地
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("Crouching"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionLanding);
+                m_seDelay = 0.4f;
+            }
+        }
+        //スライド
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionSlide);
+                m_seDelay = 0.82f;
+            }
+        }
+        //ロングスライド
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("LongSlider"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionSlide);
+                m_seDelay = 1;
+            }
+        }
+        //ヴォルト
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("Vault"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionVault);
+                m_seDelay = 0.45f;
+            }
+        }
+        //キングヴォルト
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("KongVault"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionVault);
+                m_seDelay = 0.6f;
+            }
+        }
+        //クライム
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("Climb"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionGrab);
+                m_seDelay = 0.8f;
+            }
+        }
+        //クライムオーバー
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("ClimbOver"))
+        {
+            if (m_seDelay == 0)
+            {
+                m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_ActionGrab);
+                m_seDelay = 0.5f;
+            }
+        }
+
+        //多重再生防止のディレイ処理
+        if (m_seDelay > 0)
+        {
+            m_seDelay -= Time.deltaTime;
+        }
+        else
+        {
+            m_seDelay = 0;
+        }
+        /////////////////////////////////////////////////////////////////////////
+    }
+
+
+    /// <summary>
+    /// ThirdPersonFixedUpdate
+    /// </summary>
+    void FixedUpdate() {
 
 		// MoveStateの移動制御が走っている場合、または、外部から止められている
 		if (m_MoveState.isMove() || is_StopControl) {
@@ -277,6 +370,7 @@ public class PlayerManager : MonoBehaviour, PlayerReciever
 				if (m_MoveState.StateDictionary[m] == name) {
 					// MoveStatementのenumに変換したiと検出したタグ名を投げる
 					m_animator.SetBool("is_" + name, true);
+                    Debug.Log(name);
 					m_animator.Play(name);
 					m_MoveState.changeState(m, name);
 				}
