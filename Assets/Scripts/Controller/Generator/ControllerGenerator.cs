71d54f3126248672caaf1e46049cd9b435130838
水薙優維 <thirddelta.yu_i501@me.com>
2017-12-15 02:41:17 +0900

    コントローラ再読み込み対応

--- Assets/Scripts/Controller/Generator/ControllerGenerator.cs
+++ Assets/Scripts/Controller/Generator/ControllerGenerator.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 using UnityEngine;
 
 public class ControllerGenerator : MonoBehaviour
 {
     private static InputManagerGenerator ms_InputGenerator;
     private static bool ms_bInitedCommonController = false;
-    private static bool[] ms_bInitedController = { false, false, false, false };
+    private static bool[] ms_bGaneratedController = { false, false, false, false };
+    private static bool[] ms_RegisteredController = { false, false, false, false };
 
     //ボタン名
     private static string[] strButton =
@@ -43,79 +42,96 @@ public class ControllerGenerator : MonoBehaviour
         {
             ms_InputGenerator = new InputManagerGenerator();
         }
+        if (useController > 0)
+            return GeneratePlayerController(useController, controller);
+        else if (useController == 0)
+            return GenerateCommonController(controller);
 
-        if (useController > 0 && useController < 5 && !ms_bInitedController[useController - 1])
+        return false;
+    }
+
+    public static bool GeneratePlayerController(int userID, Controller.Controller controller)
+    {
+        if (userID < 1 || userID > 4)
         {
-            string str_Player = "Controller" + useController.ToString() + "_";
-            for (int i = 0; i < (int)Controller.Button.Max; i++)
-            {
-                //A ~ RStick
-                ms_InputGenerator.AddAxis(InputAxis.CreatePadButton(str_Player + strButton[i], "joystick " + useController.ToString() + " button " + i.ToString(), null, null, null));
-                controller.SetButton((Controller.Button)i, str_Player + strButton[i]);
-            }
+            Debug.LogError("生成可能コントローラーの範囲を超えています。");
+            return false;
+        }
 
-            for (int i = 0; i < (int)Controller.Axis.Max; i++)
+        if (ms_bGaneratedController[userID - 1] && ms_RegisteredController[userID - 1])
+        {
+            Debug.LogWarning("すでに存在するコントローラです。");
+            return false;
+        }
+        string str_Player = "Controller" + userID.ToString() + "_";
+        for (int i = 0; i < (int)Controller.Button.Max; i++)
+        {
+            //A ~ RStick
+            if (!ms_bGaneratedController[userID - 1])
+                ms_InputGenerator.AddAxis(InputAxis.CreatePadButton(str_Player + strButton[i], "joystick " + userID.ToString() + " button " + i.ToString(), null, null, null));
+            controller.SetButton((Controller.Button)i, str_Player + strButton[i]);
+        }
+
+        for (int i = 0; i < (int)Controller.Axis.Max; i++)
+        {
+            if (!ms_bGaneratedController[userID - 1])
             {
                 bool invert = false;
                 if (i == (int)Controller.Axis.Cross_y || i == (int)Controller.Axis.L_y || i == (int)Controller.Axis.R_y)
                     invert = true;
                 //LStick_x ~ DPad_y
-                ms_InputGenerator.AddAxis(InputAxis.CreatePadAxis(str_Player + strAxis[i], useController, i + 1, invert));
-                controller.SetAxis((Controller.Axis)i, str_Player + strAxis[i]);
+                ms_InputGenerator.AddAxis(InputAxis.CreatePadAxis(str_Player + strAxis[i], userID, i + 1, invert));
             }
-            ms_bInitedController[useController - 1] = true;
-
-            Debug.Log("Create controller ID:" + useController.ToString());
-
-            return true;
+            controller.SetAxis((Controller.Axis)i, str_Player + strAxis[i]);
         }
-        else if (useController == 0)
+        ms_bGaneratedController[userID - 1] = true;
+        ms_RegisteredController[userID - 1] = true;
+
+        Debug.Log("Create controller ID:" + userID.ToString());
+
+        return true;
+    }
+
+    public static bool GenerateCommonController(Controller.Controller controller)
+    {
+        string str_Player = "CommonController_";
+        for (int i = 0; i < (int)Controller.Button.Max; i++)
         {
-            string str_Player = "CommonController_";
-            for (int i = 0; i < (int)Controller.Button.Max; i++)
+            if (!ms_bInitedCommonController)
+                //A ~ RStick
+                ms_InputGenerator.AddAxis(InputAxis.CreatePadButton(str_Player + strButton[i], "joystick button " + i.ToString(), null, null, null));
+            controller.SetButton((Controller.Button)i, str_Player + strButton[i]);
+        }
+        for (int i = 0; i < (int)Controller.Axis.Max; i++)
+        {
+            if (!ms_bInitedCommonController)
             {
-                if (!ms_bInitedCommonController)
-                    //A ~ RStick
-                    ms_InputGenerator.AddAxis(InputAxis.CreatePadButton(str_Player + strButton[i], "joystick button " + i.ToString(), null, null, null));
-                controller.SetButton((Controller.Button)i, str_Player + strButton[i]);
+                bool invert = false;
+                if (i == (int)Controller.Axis.Cross_y || i == (int)Controller.Axis.L_y || i == (int)Controller.Axis.R_y)
+                    invert = true;
+                //LStick_x ~ DPad_y
+                ms_InputGenerator.AddAxis(InputAxis.CreatePadAxis(str_Player + strAxis[i], 0, i + 1, invert));
             }
-            for (int i = 0; i < (int)Controller.Axis.Max; i++)
-            {
-                if (!ms_bInitedCommonController)
-                {
-                    bool invert = false;
-                    if (i == (int)Controller.Axis.Cross_y || i == (int)Controller.Axis.L_y || i == (int)Controller.Axis.R_y)
-                        invert = true;
-                    //LStick_x ~ DPad_y
-                    ms_InputGenerator.AddAxis(InputAxis.CreatePadAxis(str_Player + strAxis[i], 0, i + 1, invert));
-                }
-                controller.SetAxis((Controller.Axis)i, str_Player + strAxis[i]);
-            }
-            ms_bInitedCommonController = true;
-            Debug.Log("Create common controller");
+            controller.SetAxis((Controller.Axis)i, str_Player + strAxis[i]);
+        }
+        ms_bInitedCommonController = true;
+        Debug.Log("Create common controller");
 
-            return true;
-        }
-        else if (ms_bInitedController[useController - 1])
-        {
-            Debug.LogError("Already exist controller ID : " + useController.ToString());
-            return false;
-        }
-        else
-        {
-            Debug.LogWarning("コントローラー生成失敗");
-            return false;
-        }
+        return true;
+    }
+
+    public static void ReleaseController(int userID)
+    {
+        ms_RegisteredController[userID - 1] = false;
     }
 
     public static void CleanInputManager()
     {
         ms_InputGenerator.ClearSetting();
     }
-#if UNITY_EDITOR
+
     public static SerializedProperty GetPropaty(string axisName, InputManagerGenerator.AxisPropaty axisPropaty)
     {
         return ms_InputGenerator.GetProperty(axisName, axisPropaty);
     }
-#endif
 }
