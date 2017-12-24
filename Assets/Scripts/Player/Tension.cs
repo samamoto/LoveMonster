abaf95c3651c345cf79592020b3ba58884f64ec4
水薙優維 <thirddelta.yu_i501@me.com>
2017-12-25 00:48:21 +0900

    世界遺産出現システム構築

--- Assets/Scripts/Player/Tension.cs
+++ Assets/Scripts/Player/Tension.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
 /// <summary>
 /// プレイヤーのテンションを管理する
 /// 1.コンボシステムのコンボ数に応じて溜まり方が変わる
@@ -13,122 +14,136 @@ using UnityEngine.EventSystems;
 /// 3.テンションが規定値全員溜まったらステージに移行する(テレポ式)
 /// </summary>
 
+public class Tension : MonoBehaviour
+{
+    private const int MAX_STEP = 50;    // ステップ数
+    private const int MAX_PHASE = 6;
+    private const float TENSION_RATE = 0.55f;
 
+    private float[] TensionRateMinus = new float[MAX_PHASE / 2]; // 減りやすさを3段階くらいで分ける
+    private float[] TensionRatePlus = new float[MAX_PHASE - MAX_PHASE / 2]; // 増えやすさを3段階くらいで分ける
 
-public class Tension : MonoBehaviour {
+    public int m_TensionPhase = 0;
+    public int m_TensionCount = 0;
+    private float timeCount = 0f;
+    private int m_id;
 
-	private const int MAX_STEP = 50;    // ステップ数
-	private const int MAX_PHASE = 6;
-	private const float TENSION_RATE = 0.55f;
+    [SerializeField] private float TensionRatio = 0.0f;
 
-	private float[] TensionRateMinus = new float[MAX_PHASE/2]; // 減りやすさを3段階くらいで分ける
-	private float[] TensionRatePlus = new float[MAX_PHASE- MAX_PHASE / 2]; // 増えやすさを3段階くらいで分ける
+    private Gauge m_Gauge;
 
-	public int m_TensionPhase = 0;
-	public int m_TensionCount = 0;
-	private float timeCount = 0f;
-	private int m_id;
+    // Use this for initialization
+    private void Start()
+    {
+        m_id = GetComponent<PlayerManager>().getPlayerID();
+        // GanbaruGauge_1P~4P
+        m_Gauge = GameObject.Find("GanbaruGauge_" + m_id.ToString() + "P").GetComponent<Gauge>();
+        for (int i = 0; i < MAX_PHASE / 2; i++)
+        {
+            TensionRateMinus[i] = 1.0f + (i * (TENSION_RATE * 1.5f));    // 1.0 + 0~1*(0.5*1.5) = 1.0 ~1.75
+        }
+        // 増加値
+        for (int i = 0; i < MAX_PHASE - MAX_PHASE / 2; i++)
+        {
+            TensionRatePlus[i] = (TENSION_RATE * (MAX_PHASE / 2 + 1)) - (i * TENSION_RATE);    // 0.5*(5/2+1) - 0~2*0.5 = 1.5~0.5
+        }
+    }
 
-	private Gauge m_Gauge;
+    // Update is called once per frame
+    private void Update()
+    {
+        countTIme();
 
-	// Use this for initialization
-	void Start () {
-		m_id = GetComponent<PlayerManager>().getPlayerID();
-		// GanbaruGauge_1P~4P
-		m_Gauge = GameObject.Find("GanbaruGauge_" + m_id.ToString() + "P").GetComponent<Gauge>();
-		for (int i = 0; i < MAX_PHASE / 2; i++) {
-			TensionRateMinus[i] = 1.0f + (i * (TENSION_RATE*1.5f));    // 1.0 + 0~1*(0.5*1.5) = 1.0 ~1.75
-		}
-		// 増加値
-		for (int i = 0; i < MAX_PHASE- MAX_PHASE/ 2; i++) {
-			TensionRatePlus[i] = (TENSION_RATE*(MAX_PHASE/2+1)) - (i * TENSION_RATE);    // 0.5*(5/2+1) - 0~2*0.5 = 1.5~0.5
-		}
-	}
+        // ゲージに現在のコンボ比率を送る(テンション的なの)
+        //DebugPrint.print(m_TensionCount);
+        ExecuteEvents.Execute<GaugeReciever>(
+            target: m_Gauge.gameObject,
+            eventData: null,
+            functor: (reciever, y) => reciever.ReceivePlayerGauge(m_id, (1.0f / (MAX_STEP)) * m_TensionCount)
+        );
+        this.TensionRatio = getTensionRatio();
+    }
 
-	// Update is called once per frame
-	void Update () {
-		countTIme();
+    private void countTIme()
+    {
+        timeCount += Time.deltaTime;
+        // 下降状況か、上昇か処理を分ける
+        if (m_TensionPhase < MAX_PHASE / 2)
+        {
+            // 各Phaseごとに定められた規定時間を越えたらテンション下げる
+            if (timeCount >= TensionRateMinus[m_TensionPhase])
+            {
+                timeCount = 0f;
+                if (m_TensionCount > 0)
+                {
+                    m_TensionCount--;
+                }
+            }
+        }
+        else
+        {
+            // 増加
+            if (timeCount >= TensionRatePlus[(m_TensionPhase) - (MAX_PHASE / 2)])
+            {
+                timeCount = 0f;
+                if (m_TensionCount < MAX_STEP)
+                {
+                    m_TensionCount++;
+                }
+            }
+        }
+    }
 
-		// ゲージに現在のコンボ比率を送る(テンション的なの)
-		//DebugPrint.print(m_TensionCount);
-		ExecuteEvents.Execute<GaugeReciever>(
-			target: m_Gauge.gameObject,
-			eventData: null,
-			functor: (reciever, y) => reciever.ReceivePlayerGauge(m_id, (1.0f / (MAX_STEP)) * m_TensionCount)
-		);
-		Debug.Log(getTensionRatio());
+    /// <summary>
+    /// 外部(コンボシステム側)からPhaseを切り替えるときに使用する
+    /// </summary>
+    public void updateTensionPhase(int cnt)
+    {
+        int count = cnt / 2;
 
-	}
+        m_TensionPhase = count; // 段階分けて格納
 
-	void countTIme() {
-		timeCount += Time.deltaTime;
-		// 下降状況か、上昇か処理を分ける
-		if (m_TensionPhase < MAX_PHASE / 2) {
-			// 各Phaseごとに定められた規定時間を越えたらテンション下げる
-			if (timeCount >= TensionRateMinus[m_TensionPhase]) {
-				timeCount = 0f;
-				if (m_TensionCount > 0) {
-					m_TensionCount--;
-				}
+        // countがこちらの最大値より大きい場合は修正する
+        if (count > MAX_PHASE)
+        {
+            count = MAX_PHASE;
+        }
+        // 範囲チェック
+        if (m_TensionPhase < 0)
+        {
+            m_TensionPhase = 0;
+            // 配列の範囲外エラー防止のため-1
+        }
+        else if (m_TensionPhase > MAX_PHASE - 1)
+        {
+            m_TensionPhase = MAX_PHASE - 1;
+        }
+    }
 
-			}
-		} else {
-			// 増加
-			if(timeCount >= TensionRatePlus[(m_TensionPhase) - (MAX_PHASE / 2)]){
-				timeCount = 0f;
-				if(m_TensionCount < MAX_STEP) {
-					m_TensionCount++;
-				}
-			}
+    /// <summary>
+    ///テンションのクリア
+    /// </summary>
+    public void resetTension()
+    {
+        m_TensionPhase = 0;
+        m_TensionCount = 0;
+        timeCount = 0f;
+    }
 
-		}
-	}
-	/// <summary>
-	/// 外部(コンボシステム側)からPhaseを切り替えるときに使用する
-	/// </summary>
-	public void updateTensionPhase(int cnt) {
+    /// <summary>
+    /// 落下したときなど、テンションを下降させる
+    /// </summary>
+    public void downTension()
+    {
+        m_TensionPhase = 0;
+        timeCount = 0;
+    }
 
-		int count = cnt / 2;
-
-
-		m_TensionPhase = count; // 段階分けて格納
-
-		// countがこちらの最大値より大きい場合は修正する
-		if (count > MAX_PHASE) {
-			count = MAX_PHASE;
-		}
-		// 範囲チェック
-		if(m_TensionPhase < 0) {
-			m_TensionPhase = 0;
-		// 配列の範囲外エラー防止のため-1
-		} else if(m_TensionPhase > MAX_PHASE-1) {
-			m_TensionPhase = MAX_PHASE-1;
-		}
-	}
-
-	
-	/// <summary>
-	///テンションのクリア
-	/// </summary>
-	public void resetTension() {
-		m_TensionPhase = 0;
-		m_TensionCount = 0;
-		timeCount = 0f;
-	}
-
-	/// <summary>
-	/// 落下したときなど、テンションを下降させる
-	/// </summary>
-	public void downTension() {
-		m_TensionPhase = 0;
-		timeCount = 0;
-	}
-
-	/// <summary>
-	/// 現在のテンションの割合を返す
-	/// </summary>
-	public float getTensionRatio() {
-		return ((float)m_TensionCount / MAX_STEP);
-	}
-
+    /// <summary>
+    /// 現在のテンションの割合を返す
+    /// </summary>
+    public float getTensionRatio()
+    {
+        return ((float)m_TensionCount / MAX_STEP);
+    }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,30 +7,30 @@ using UnityEngine.SceneManagement;
 /// それぞれのUIを管理するScript
 /// PlayerのUI用のScriptは表示のみ(中身の構成)を
 /// 行い、値の変動はこちらで管理する。
-/// 
+///
 /// 全体で共有している表示(現在の案だと画面上部のStartからGoalまでの道のりの表示)
 /// を管理する。
-/// 
+///
 /// </summary>
-
-
 
 public class UIManager : MonoBehaviour
 {
+    private MainGameManager m_Game;
+    private TimeManager m_Time;
+    private CountDownSystem m_Count;
 
-	private MainGameManager m_Game;
-	private TimeManager m_Time;
-	private CountDownSystem m_Count;
     public void Start()
     {
-		m_Game = GameObject.FindWithTag("GameManager").GetComponent<MainGameManager>();
-		m_Time = GameObject.Find("TimeManager").GetComponent<TimeManager>();
-		m_Count = GameObject.Find("CountDownSystem").GetComponent<CountDownSystem>();
-	}
+        m_Game = GameObject.FindWithTag("GameManager").GetComponent<MainGameManager>();
+        m_Time = GameObject.Find("TimeManager").GetComponent<TimeManager>();
+        m_Count = GameObject.Find("CountDownSystem").GetComponent<CountDownSystem>();
+    }
 
     public void Update()
     {
-		
     }
 
+    public void UIenable(bool enable)
+    {
+    }
 }
