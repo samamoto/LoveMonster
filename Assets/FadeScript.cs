2001e93255e5a7b13ad994d4b71b68bb9897032f
OyamaAtsushi <blazwel@gmail.com>
2018-01-02 18:32:55 +0900

    タイトル画面

---
+++ Assets/FadeScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FadeScript : MonoBehaviour {
+
+	void Start() {
+		// ２秒後にFadeIn()を、５秒後にFadeOut()を呼び出す
+		Invoke("FadeIn", 2f);
+		Invoke("FadeOut", 5f);
+	}
+
+	void FadeIn() {
+		// SetValue()を毎フレーム呼び出して、１秒間に０から１までの値の中間値を渡す
+		iTween.ValueTo(gameObject, iTween.Hash("from", 0f, "to", 1f, "time", 1f, "onupdate", "SetValue"));
+	}
+	void FadeOut() {
+		// SetValue()を毎フレーム呼び出して、１秒間に１から０までの値の中間値を渡す
+		iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "time", 1f, "onupdate", "SetValue"));
+	}
+	void SetValue(float alpha) {
+		// iTweenで呼ばれたら、受け取った値をImageのアルファ値にセット
+		gameObject.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 0, 0, alpha);
+	}
+}
--- Assets/Scripts/SceneManager/TitleManager.cs
+++ Assets/Scripts/SceneManager/TitleManager.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
+using Controller;
+using UnityStandardAssets.ImageEffects;
 /// <summary>
 /// Scene開始後、ムービーを再生(再生中は操作不可?、スキップはアリ?)、
 /// 現在はスペースキーでSceneの移動を行う。
@@ -19,16 +19,25 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
     //スクリプト群
-    //private SceneChange m_ScreenChange;
-
-    private InputManagerGenerator inputmanage;
-
     private bool MovieEndFlag=false;  //ムービー再生終了フラグ
 	private Animator animator;
+	private AudioList m_Audio;
+	private Controller.Controller m_Controller;
+	private NoiseAndScratches m_Noise;
+	private float timeCount = 0f;
+	private bool buttonFlag;
+	public enum TitlePhase {
+		Awake,
+		Start,
+		Wait,
+		End,
+		NextScene,
+	};
 
+	TitlePhase m_Phase = TitlePhase.Awake;
 
-    // Use this for initialization
-    private void Start()
+	// Use this for initialization
+	private void Start()
     {
 		//m_ScreenChange = GameObject.Find("SceneChange").GetComponent<SceneChange>();
 
@@ -36,34 +45,100 @@ public class TitleManager : MonoBehaviour {
 		//追加シーン     ここでフェードを読み込むのもアリ
 		//SceneManager.LoadScene("TitleScene", LoadSceneMode.Additive);
 
-		animator = GameObject.Find("Player1").GetComponent<Animator>();
-		animator.SetTrigger("BHS_FlashKick");
-        //ここにムービー再生処理を追加?　→MovieTextureにフラグを追加するべき?
-        MovieEndFlag = false;       //再生フラグ初期化
-    }
+		//animator = GameObject.Find("Player1").GetComponent<Animator>();
+		//animator.SetTrigger("BHS_FlashKick");
+		//ここにムービー再生処理を追加?　→MovieTextureにフラグを追加するべき?
+		//MovieEndFlag = false;       //再生フラグ初期化
+		GameObject.Find("Camera").GetComponent<NoiseAndScratches>().enabled = true;
+		m_Audio = GameObject.Find("SoundManager").GetComponent<AudioList>();
+		m_Controller = GetComponent<Controller.Controller>();
+	}
 
-    // Update is called once per frame
-    private void Update()
+	// Update is called once per frame
+	private void Update()
     {
-		//if (MovieEndFlag == true)   //動画の再生が終了していたら
-		//{
-		//仮
-			SceneToNext();  //次のシーンへ移動(チュートリアルへ)
-        //}
+
+		switch (m_Phase) {
+		case TitlePhase.Awake:
+			m_Audio.Play((int)AudioList.SoundList_BGM.BGM_Game_Stage1);
+			m_Phase = TitlePhase.Start;
+			Debug.Log("next" + m_Phase.ToString());
+			break;
+		case TitlePhase.Start:
+			timeCount += Time.deltaTime * 2;
+			if (timeCount >= 5f) {
+				GameObject.Find("Camera").GetComponent<NoiseAndScratches>().enabled = false;
+				GameObject.Find("Camera").GetComponent<SunShafts>().enabled = true;
+				m_Phase = TitlePhase.Wait;
+				timeCount = 0f;
+				Debug.Log("next" + m_Phase.ToString());
+			} else {
+				GameObject.Find("Camera").GetComponent<NoiseAndScratches>().grainIntensityMin -= Time.deltaTime * 2;
+			}
+			break;
+		case TitlePhase.Wait:
+			timeCount += Time.deltaTime;
+			iTween.ValueTo(gameObject, iTween.Hash(
+				"from", 40f,
+				"to", 3.43f,
+				"time", 1f,
+				"onupdate", "SetIntensity",  // 毎フレーム SetAlpha() を呼びます。
+				"easetype", "easeOutCubic"
+				));
+			if (timeCount >= 3f) {
+				timeCount = 0f;
+				m_Phase++;
+				Debug.Log("next" + m_Phase.ToString());
+			}
+			break;
+		case TitlePhase.End:
+			if (m_Controller.GetButtonDown(Button.B) || m_Controller.GetButtonDown(Button.A) || m_Controller.GetButtonDown(Button.Menu)) {
+				m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_UI_Title_Decision);
+				m_Phase = TitlePhase.NextScene;
+				Debug.Log("next" + m_Phase.ToString());
+			}
+			break;
+		case TitlePhase.NextScene:
+			SceneToNext();
+			break;
+
+
+		}
     }
 
     public void SceneToNext()     //次のシーンへ移動
     {
         //m_ScreenChange._DebugInput();
-        if (Input.GetKeyUp(KeyCode.Space))//スペースキーで
-        {
-			//移動したいシーンへ移動(チュートリアルシーン)
-			//SceneManager.LoadScene("TutorialScene");
-			SceneChange.Instance._SceneLoadTutorial();
-               
-        }
+		//移動したいシーンへ移動(チュートリアルシーン)
+		//SceneManager.LoadScene("TutorialScene");
+		timeCount += Time.deltaTime;
+		//iTween.FadeTo(GameObject.Find("Panel"), 1f, 2f);
+		SetFade(timeCount/3);
+		//iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "time", 1f, "onupdate", "SetFade"));
+		/*
+		iTween.ValueTo(gameObject, iTween.Hash(
+			"from", 0f,
+			"to", 1f,
+			"time", 2f,
+			"looptype", iTween.LoopType.none,
+			"onupdate", "SetFade",  // 毎フレーム SetAlpha() を呼びます。
+			"onupdatetarget", GameObject.Find("Fade")
+			));
+		*/
+		if (timeCount > 3.0f) {
+			//SceneChange.Instance._SceneLoadTutorial();
+			SceneChange.Instance._SceneLoadGame();
+		}
+	}
 
-
-    }
-
+	void SetIntensity(float value) {
+		GameObject.Find("Camera").GetComponent<SunShafts>().sunShaftIntensity = value;
+	}
+	void SetFade(float value) {
+		float v = value;
+		if(v >= 1) {
+			v = 1f;
+		} 
+		GameObject.Find("Fade").GetComponent<UnityEngine.UI.Image>().color = new Color(0,0,0, v);
+	}
 }
