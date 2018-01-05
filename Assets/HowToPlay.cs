84887a1b52aad933118950ec6accf7906cb7f407
OyamaAtsushi <blazwel@gmail.com>
2018-01-06 03:18:43 +0900

    チュートリアルを少し変更

--- Assets/HowToPlay.cs
+++ Assets/HowToPlay.cs
@@ -7,7 +7,7 @@ using Controller;
 
 public class HowToPlay : MonoBehaviour {
 
-
+	private AudioList m_Audio;
     private Controller.Controller m_Controller;
 
 
@@ -17,13 +17,15 @@ public class HowToPlay : MonoBehaviour {
     private GameObject Image1;
 
 
-    //Images.Length     //最大値
+	//Images.Length     //最大値
 
+	private float timeCount = 0;
 
     private int count=0;    //カウント
     // Use this for initialization
     void Start () {
-
+		m_Audio = GameObject.Find("SoundManager").GetComponent<AudioList>();
+		m_Audio.Play((int)AudioList.SoundList_BGM.BGM_Tutorial0);
         m_Controller = GameObject.Find("TutorialManager").GetComponent<Controller.Controller>();
 
         count =0;      //初期化
@@ -33,29 +35,47 @@ public class HowToPlay : MonoBehaviour {
 
         Image1.GetComponent<Image>().sprite = Images[count];   //画像切り替え
     }
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 
-
+		// フェードを待つ
+		if (timeCount > 0) {
+			timeCount += Time.deltaTime;
+			SetFade(timeCount / 3);
+			if(timeCount > 3) {
+				SceneChange.Instance._SceneLoadGame();
+			}
+		}
 
         //最後まで画像が進んだとき
         if (count + 1 == Images.Length)
         {
-            if (m_Controller.GetButtonDown(Controller.Button.A))
+            if (m_Controller.GetButtonDown(Controller.Button.A) || m_Controller.GetButtonDown(Controller.Button.B))
             {
                 //シーンの移動
-                SceneChange.Instance._SceneLoadGame();
-            }
-        }
+                //SceneChange.Instance._SceneLoadGame();
+				m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_UI_Title_Decision);
+				timeCount += Time.deltaTime;
+			}
+		}
 
-        //ボタンが押されたら次の画像に
-        if (m_Controller.GetButtonDown(Controller.Button.A))
-        {
-            count++;
+		//ボタンが押されたら次の画像に
+		if (m_Controller.GetButtonDown(Controller.Button.A) || m_Controller.GetButtonDown(Controller.Button.B)){
+			m_Audio.PlayOneShot((int)AudioList.SoundList_SE.SE_UI_Menu_Decision);
+			count++;
             Image1.GetComponent<Image>().sprite = Images[count];  //画像切り替え
         }
 
         
     }
+
+	void SetFade(float value) {
+		float v = value;
+		if (v >= 1) {
+			v = 1f;
+		}
+		GameObject.Find("Fade").GetComponent<UnityEngine.UI.Image>().color = new Color(0, 0, 0, v);
+	}
+
 }
