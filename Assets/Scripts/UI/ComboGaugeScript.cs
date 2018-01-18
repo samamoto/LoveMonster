e8ac39b9ff85801efdfc0c05289f2c2e55efb6ae
Oyama Atsushi <32250363+bluemistel@users.noreply.github.com>
2018-01-18 20:41:36 +0900

    ソリューションから見つからなかったので手動で追加

---
+++ Assets/Scripts/UI/ComboGaugeScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ComboGaugeScript : MonoBehaviour {
+    
+    public int initComboLevel = 0;
+    public int comboLevels = 5; //合計のコンボの段階数
+	public int nowCombo = 0;
+    public float comboTime = 5.0f; //コンボが一段階落ちるまでの許す時間
+    private float[] gaugeFillAmounts = { 0.0f, 0.154f, 0.31f, 0.455f, 0.6f, 0.8f, 1.0f}; //コンボ段階に合わせたそれぞれのゲージの貯まる量
+    public Image fill;
+    public Image fillGlow;
+
+	public float fillAmout = 0;
+	public float nowFill = 0;
+	public float MaxFillAmout = 0;
+	float timeCount = 0f;
+
+    // Use this for initialization
+    void Start () {
+        UpdateComboLevel(initComboLevel, comboTime);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeCount += Time.deltaTime;
+
+        if (fill.fillAmount > 0f && nowCombo > 0)
+        {
+
+			//     nowf 
+			// ||||----|f
+			nowFill = gaugeFillAmounts[nowCombo] - gaugeFillAmounts[nowCombo-1];
+			float f = gaugeFillAmounts[nowCombo-1];
+			float t = timeCount / comboTime;
+
+			// f + nowFill = 現在の比率
+			fillAmout = f + (nowFill - t*nowFill);
+			// 
+			//fillAmout -= 1.0f / (comboTime*comboLevels) * Time.deltaTime;
+
+			fill.fillAmount = fillAmout;
+			fillGlow.fillAmount = fillAmout;
+
+		}
+
+		if (nowCombo <= 0) {
+			fill.fillAmount = fillGlow.fillAmount = 0f;
+		}
+
+	}
+
+	//comboLevel に対してゲージの貯まる量を調整する。comboLevel　はコンボの何段階目（0-5まで）。
+	public void UpdateComboLevel(int comboLevel, float time) {
+
+		int level = comboLevel;
+
+		if(level > gaugeFillAmounts.Length-1) {
+			level = gaugeFillAmounts.Length - 1;
+		}
+		nowCombo = level;
+
+		comboTime = time;
+		timeCount = 0;
+
+		fillAmout = gaugeFillAmounts[nowCombo];
+		fill.fillAmount = gaugeFillAmounts[nowCombo];
+		fillGlow.fillAmount = gaugeFillAmounts[nowCombo];
+	}
+
+	public void UpdateComboLevel(int comboLevel) {
+		// 現在最大量
+		for (int i = 0; i < nowCombo; i++) {
+			MaxFillAmout += gaugeFillAmounts[i + 1];
+		}
+		fill.fillAmount = gaugeFillAmounts[comboLevel];
+		fillGlow.fillAmount = gaugeFillAmounts[comboLevel];
+	}
+
+	public void setComboLevel(int level) {
+		nowCombo = level;
+	}
+
+
+}
---
+++ Assets/Scripts/UI/CountDownTimer.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class CountDownTimer : MonoBehaviour {
+	private int minute; //分
+	private float second;   //秒
+	private float second_2;   //秒　下2桁
+	private int oldSecond;
+	private bool timerFlag = false;
+	private char textField;
+	private bool countUpDownSwitch = true;
+	public float CountDownLimit = 90;
+
+	// Use this for initialization
+	void Start() {
+
+		if (CountDownLimit <= 0) {
+			CountDownLimit = 45;
+		}
+		minute = 0;
+		second = 0;
+		oldSecond = 0;
+		int cnt = 0;
+		while ((int)CountDownLimit / 60 > 0) {
+			minute += (int)CountDownLimit / 60;
+			CountDownLimit -= 60f;
+			cnt++;
+		}
+		second = CountDownLimit;
+	}
+
+	private void restart() {
+		if (CountDownLimit <= 0) {
+			CountDownLimit = 45;
+		}
+		minute = 0;
+		second = 0;
+		oldSecond = 0;
+		int cnt = 0;
+		while ((int)CountDownLimit / 60 > 0) {
+			minute += (int)CountDownLimit / 60;
+			CountDownLimit -= 60f;
+			cnt++;
+		}
+		second = CountDownLimit;
+	}
+
+
+	// Update is called once per frame
+	void Update() {
+		//countTime += Time.deltaTime; //スタートしてからの秒数を格納
+		//minute = Convert.ToInt32(countTime) % 60;  //分の割り出し
+		//countTime = countTime - 60 * minute;   //秒の割り出し
+		//second = Convert.ToInt32(countTime);        //秒を格納
+
+
+
+		//GetComponent<Text>().text = countTime.ToString("F2"); //小数2桁にして表示
+		if (Time.timeScale > 0) {
+			// カウントダウンが開始されていなければ止める
+			if (!timerFlag)
+				return;
+
+			if (countUpDownSwitch) { 
+				second -= Time.deltaTime;
+				if (second <= 0.0f) {
+					if (minute > 0) {
+						minute--;
+						second = second + 60.0f;   //秒を0に戻す
+					}
+				}
+			}
+			GetComponent<TextMeshProUGUI>().outlineColor = new Color(0f, 1f, 0.2f);
+			GetComponent<TextMeshProUGUI>().text = minute.ToString("00") + "." + second.ToString("F2");
+
+		}
+
+
+	}
+
+	/// <summary>
+	/// カウントを開始する
+	/// </summary>
+	public void startTimer() {
+		timerFlag = true;
+	}
+
+	/// <summary>
+	/// カウントを一時停止する
+	/// </summary>
+	public void stopTimer() {
+		timerFlag = false;
+	}
+
+	/// <summary>
+	/// カウントをリセットする
+	/// </summary>
+	public void resetTimer() {
+		timerFlag = false;
+		minute = 0;
+		second = 0;
+		oldSecond = 0;
+	}
+
+	/// <summary>
+	/// 現在時間を文字列で返す
+	/// </summary>
+	/// <returns>表示時間の文字列</returns>
+	public string getTimeString() {
+		string s = minute.ToString("00") + ":" + Convert.ToInt32(second).ToString("00");
+		return s;
+	}
+
+	public void setTimer(float time) {
+		CountDownLimit = time;
+		restart();
+	}
+
+	public bool isCountDownEnd() {
+		if (second <= 0 && minute <= 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
